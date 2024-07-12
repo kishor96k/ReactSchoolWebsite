44fefb385153e2a94f21223db8f50c2880f052cc
kishor96k <chavankishor813@gmail.com>
2024-07-12 09:37:36 +0530

    added the basic folder structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <p>its navbar</p>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>
+            <p>its sidebar</p>
+        </div>
+    );
+};
+
+export default Sidebar;
