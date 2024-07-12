b0fc6bffa7ee8a535bdb9399f3aeb52523e7f22b
Jags3054 <patilj922@gmail.com>
2024-07-12 16:40:47 +0530

    Home page carosel changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,52 +1,46 @@
 import React from 'react';
 import '../Home/Home.css'
+import student from '../../assets/images/lake-7301021_1280.jpg'
+import student2 from '../../assets/images/sunset-7708222_1280.jpg'
 const Home = () => {
     return (
         <div>
-         <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <div class="container">
+ 
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
     <ol class="carousel-indicators">
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
       <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
     </ol>
 
-
-    <div class="carousel-inner" role="listbox">
+   
+    <div class="carousel-inner">
       <div class="item active">
-        <img src="https://placehold.it/1200x400?text=IMAGE" alt="Image"/>
-        <div class="carousel-caption">
-          <h3>Sell $</h3>
-          <p>Money Money.</p>
-        </div>      
+        <img src={student} alt="Los Angeles" style={{width:"100%"}}/>
       </div>
 
       <div class="item">
-        <img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="Image"/>
-        <div class="carousel-caption">
-          <h3>More Sell $</h3>
-          <p>Lorem ipsum...</p>
-        </div>      
+        <img src={student} alt="Chicago" style={{width:"100%"}}/>
+      </div>
+    
+      <div class="item">
+        <img src={student} alt="New york"  style={{width:"100%"}}/>
       </div>
     </div>
 
-
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+ 
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
       <span class="sr-only">Next</span>
     </a>
+  </div>
 </div>
-  
-<div class="container text-center">    
- 
-</div>
-
-<footer class="container-fluid text-center">
-  <p>Footer Text</p>
-</footer>
         </div>
     );
 };
