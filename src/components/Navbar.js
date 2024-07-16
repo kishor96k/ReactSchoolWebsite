f93b881c05a54c37e847402604deacabd6fc5b9f
Jags3054 <patilj922@gmail.com>
2024-07-16 13:07:06 +0530

    navbar chnages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-primary" style={{background:" #94b8b8"}}>
-      <Link className="navbar-brand" to="/">UNIQUE ENGLISH MEDIUM SCHOOL</Link>
+      <Link className="navbar-brand" to="/">Hogwards International SCHOOL</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -23,12 +23,46 @@ export default function Navbar() {
           <li className="nav-item">
             <Link className="nav-link" to="/blog">Blog</Link>
           </li>
-          <li className="nav-item">
+
+          
+        <li className="nav-item">
             <Link className="nav-link" to="/login">Login</Link>
           </li>
           <li className="nav-item">
             <Link className="nav-link" to="/registration">Registration</Link>
           </li>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+        <span className="navbar-toggler-icon"></span>
+        </button>
+<li className="nav-item dropdown">
+                    <a
+                        className="nav-link dropdown-toggle"
+                        href="#"
+                        id="navbarDropdown"
+                        role="button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                    >
+                        More
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Events</a>
+                        <a className="dropdown-item" href="#">Programs</a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="#">Gallery</a>
+                    </div>
+                </li>
+                
         </ul>
       </div>
     </nav>
