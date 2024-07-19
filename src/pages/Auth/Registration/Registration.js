27b52ddecd1d492be701a0380f188ecf7c819a8f
kishor96k <chavankishor813@gmail.com>
2024-07-19 15:11:31 +0530

    kishor final changes for all forms validation

--- src/pages/Auth/Registration/Registration.js
+++ src/pages/Auth/Registration/Registration.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import { useState, useEffect } from "react";
 import studimg from "../../../assets/images/studentone.jpg";
